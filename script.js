2f7ee008f7ef9ebc4bcda8b1f25a048dbbc6d706
Coder_Sam_049 <codewithsam049@gmail.com>
2024-07-08 16:45:53 +0530

    add javascript some method

---
+++ script.js
@@ -0,0 +1,18 @@
+const learners=document.querySelector("#third > div > div.learner > span");
+const subscribers= document.querySelector("#third > div > div.subscriber > p > span")
+
+let starno=0;
+
+function increaseno(starcount,users,lastcount,starcountplus){
+const subcount= setInterval(()=>{
+starcount+=starcountplus;
+users.innerHTML=starcount;
+if(starcount==lastcount){
+clearInterval(subcount)
+}
+},)
+}
+
+
+increaseno(starno,learners,50000,80);
+increaseno(starno,subscribers,1000000,400)
